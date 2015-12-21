af4c8684c574ab0488a433533d822df75e23f3b4
Jesus Rincon <00jjessejj00@gmail.com>
2015-12-21 16:38:14 -0430

    toggle finished

--- js/toggled.js
+++ js/toggled.js
@@ -1,1 +1,10 @@
-console.log('hola');
+function toggleProducts(ProductID) {
+  console.log('Acabo de clickear ' + ProductID);
+  $('.portfolio-item').hide();
+  $('.portfolio-item'+'.'+ProductID).show();
+
+}
+function mainToggle(){
+  $('.portfolio-item').show();
+  $('ul.dropdown-menu li.active').removeClass('active');
+}
